ef8eef4484e763d7543d2908532e4f7ca5122369
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-13 11:48:13 +0200

    beautifull screen update

---
+++ src/components/BeautifulScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BeautifulScreen = () => {
+    return (
+        <div className="beautifull-screen">
+            <input type="text" readOnly/>
+        </div>
+    )
+}
+
+export default BeautifulScreen;
