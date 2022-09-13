0b82e31a572de7ce2851d3b19cc9f83325813d75
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-13 15:28:44 +0200

    greatOperationBttn

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,9 @@
 //Import React (Mandatory Step).
 import React from 'react';
 import CalculatorTitle from './CalculatorTitle.js';
+import BeautifulScreen from './BeautifulScreen.js';
+import GreatOperationButton from './GreatOperationButton.js';
+import MagniﬁcientEqualButton from './MagniﬁcientEqualButton.js';
 
 class Calculator extends React.Component {
     render()
