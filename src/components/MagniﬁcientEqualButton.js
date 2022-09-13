55cdca46bce6604fbda659186d547fd98f654ef6
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-13 15:30:36 +0200

    MagnificientEqualButton added

---
+++ src/components/MagniﬁcientEqualButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+  
+const MagnificientEqualButton = () => {
+  return (
+    <input
+      type="button"
+      value= "="
+    />
+  );
+}
+  
+// Export our button component.
+export default MagnificientEqualButton;
