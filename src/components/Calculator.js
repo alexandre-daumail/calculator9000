d73e24f4c27d821b3cbe9fb73b5d3c6565cbdd06
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-13 11:16:37 +0200

    fixed import bugs

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+//Import React (Mandatory Step).
+import React from 'react';
+
+class Calculator extends React.Component {
+    render()
+    {
+        return "Calculator9000";
+    }
+}
+
+export default Calculator;
