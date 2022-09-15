7a5e0dd30408429636c0659e2c0ae659725ccc32
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-15 09:13:55 +0200

    simple buttons ok

--- src/App.js
+++ src/App.js
@@ -6,19 +6,35 @@ import BeautifulScreen from './components/BeautifulScreen';
 import ButtonBox from './components/ButtonBox';
 import Button from './components/Button';
 
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
 const App = () => {
   return (
     <Calculator>
-      <CalculatorTitle/>
+      <CalculatorTitle />
       <BeautifulScreen value="0" />
       <ButtonBox>
-        <Button
-          className=""
-          value="0"
-          onClick={() => {
-            console.log("Button clicked!");
-          }}
-        />
+        {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={() => {
+                  console.log(`${btn} clicked!`);
+                }}
+              />
+            );
+          })
+        }
       </ButtonBox>
     </Calculator>
   );
