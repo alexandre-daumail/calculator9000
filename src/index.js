1d62113298216eddbd9a1877bc6351609688ef48
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-15 08:54:55 +0200

    index update

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import ReactDOM from "react-dom";
 import App from './App';
 import './index.css';
 
-
-const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  rootElement
+  document.getElementById("root")
 );
