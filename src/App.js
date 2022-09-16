f84f31d39598d0000f1bc3e883654f7f8f6f6ff8
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-16 08:53:33 +0200

    calculator becomes smart component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,9 @@
-import React, { useState } from "react";
-
 import Calculator from './components/Calculator.js';
-import CalculatorTitle from './components/CalculatorTitle';
-import BeautifulScreen from './components/BeautifulScreen';
-import ButtonBox from './components/ButtonBox';
-import AmazingNumberButton from './components/AmazingNumberButton';
-import MagnificientEqualButton from './components/MagnificientEqualButton';
-import GreatOperationButton from "./components/GreatOperationButton.js";
-
-const toLocaleString = (num) =>
-  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
-
-const removeSpaces = (num) => num.toString().replace(/\s/g, "");
 
 const App = () => {
 
-  let [calc, setCalc] = useState({
-    sign: "",
-    num: 0,
-    res: 0,
-  });
+  return (<Calculator/>);
 
-  const handleClickParent = (number) => {
-
-    if (removeSpaces(calc.num).length < 16) {
-      setCalc({
-        ...calc,
-        num:
-          calc.num === 0 && number === "0"
-            ? "0"
-            : removeSpaces(calc.num) % 1 === 0
-              ? toLocaleString(Number(removeSpaces(calc.num + number)))
-              : toLocaleString(calc.num + number),
-        res: !calc.sign ? 0 : calc.res,
-      });
-    }
-  }
-
-  return (
-    <Calculator>
-
-      <CalculatorTitle />
-
-      <BeautifulScreen value={calc.num ? calc.num : calc.res} />
-
-      <ButtonBox>
-
-        <AmazingNumberButton handleClickParent={handleClickParent} />
-        <GreatOperationButton />
-        <MagnificientEqualButton />
-
-      </ButtonBox>
-    </Calculator>
-  );
 };
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,61 @@
-import "./Calculator.css";
+import React, { useState } from "react";
 
-const Calculator = require = ({ children }) => {
-  return <div className="calculator">{children}</div>;
+import "./Calculator.css";
+import CalculatorTitle from './CalculatorTitle';
+import BeautifulScreen from './BeautifulScreen';
+import ButtonBox from './ButtonBox';
+import AmazingNumberButton from './AmazingNumberButton';
+import MagnificientEqualButton from './MagnificientEqualButton';
+import GreatOperationButton from "./GreatOperationButton.js";
+
+const toLocaleString = (num) =>
+  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+
+const Calculator = () => {
+
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
+  const handleClickParent = (number) => {
+
+    if (removeSpaces(calc.num).length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && number === "0"
+            ? "0"
+            : removeSpaces(calc.num) % 1 === 0
+              ? toLocaleString(Number(removeSpaces(calc.num + number)))
+              : toLocaleString(calc.num + number),
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+  }
+
+  return (
+
+    <div className="calculator">
+
+      <CalculatorTitle />
+
+      <BeautifulScreen value={calc.num ? calc.num : calc.res} />
+
+      <ButtonBox>
+
+        <AmazingNumberButton handleClickParent={handleClickParent} />
+        <GreatOperationButton />
+        <MagnificientEqualButton />
+
+      </ButtonBox>
+
+    </div>
+  )
+
 };
 
 export default Calculator;
