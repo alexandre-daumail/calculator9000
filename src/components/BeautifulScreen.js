e4e6c2e2ee58c4310b0601874b18bb90c196b6cd
Alexandre DAUMAIL <alexandre.daumail@laplateforme.io>
2023-09-21 02:57:57 +0200

    fix

--- src/components/BeautifulScreen.js
+++ src/components/BeautifulScreen.js
@@ -1,11 +1,10 @@
-import { Textfit } from "react-textfit";
 import "../css/BeautifulScreen.css";
 
 const BeautifulScreen = ({ value }) => {
     return (
-        <Textfit className="beautiful-screen" mode="single" max={70}>
+        <button className="beautiful-screen" mode="single" max={70}>
             {value}
-        </Textfit>
+        </button>
     );
 };
 
